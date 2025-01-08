a7b846e61a63fdd8ed4ca083be5f1c34c443f0b7
Diwaker <diwamishra21@gmail.com>
2025-01-08 08:29:54 +0530

    my first change

---
+++ include/config.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "diwaker";
+
+
+// Create connection
+//$connect = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+//if ($connect->connect_error) {
+   // die("Connection failed: " . $query->connect_error);
+//} 
+//else
+?>
